eec54c7586b8083544f1f294fe4477e9dc89f636
Alonso Acosta <alonso.ae@hotmail.com>
2025-06-20 13:46:34 -0600

    first commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Alonso Acosta Enriquez on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Alonso Acosta Enriquez on 17/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .foregroundStyle(.tint)
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
